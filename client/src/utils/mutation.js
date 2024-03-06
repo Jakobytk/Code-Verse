76d9f73cf8827462c6ff3746a50b810909b0ce74
jakobytk <you@example.com>
2024-03-05 19:59:24 -0600

    mutations and queries

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,15 +1,75 @@
-const { gql } = require('apollo-server-express');
-
-const typeDefs = gql`
-  type Mutation {
-    addProfile(name: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile: Profile
-    removeSkill(skill: String!): Profile
-    addThought(thoughtId: ID!, thought: String!): Profile
-    removeThought(thought: String!): Thought
+export const ADD_PROFILE = `
+  mutation addProfile($name: String!, $email: String!, $password: String!) {
+    addProfile(name: $name, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+        email
+        skills
+      }
+    }
   }
 `;
 
-module.exports = typeDefs;
+export const LOGIN = `
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+        email
+        skills
+      }
+    }
+  }
+`;
+
+export const ADD_SKILL = `
+  mutation addSkill($profileId: ID!, $skill: String!) {
+    addSkill(profileId: $profileId, skill: $skill) {
+      _id
+      name
+      email
+      skills
+    }
+  }
+`;
+
+export const REMOVE_PROFILE = `
+  mutation removeProfile {
+    removeProfile {
+      _id
+    }
+  }
+`;
+
+export const REMOVE_SKILL = `
+  mutation removeSkill($skill: String!) {
+    removeSkill(skill: $skill) {
+      _id
+      name
+      email
+      skills
+    }
+  }
+`;
+
+export const ADD_THOUGHT = `
+  mutation addThought($thoughtId: ID!, $thought: String!) {
+    addThought(thoughtId: $thoughtId, thought: $thought) {
+      _id
+      name
+    }
+  }
+`;
+
+export const REMOVE_THOUGHT = `
+  mutation removeThought($thought: String!) {
+    removeThought(thought: $thought) {
+      _id
+      name
+    }
+  }
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,13 +1,52 @@
-const { gql } = require('apollo-server-express');
-
-const typeDefs = gql`
-  type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
-    me: Profile
-    thoughts: [Thought]!
-    thought(thoughtId: ID!): Thought!
+export const PROFILES = `
+  query profiles {
+    profiles {
+      _id
+      name
+      email
+      skills
+    }
   }
 `;
 
-module.exports = typeDefs;
+export const PROFILE = `
+  query profile($profileId: ID!) {
+    profile(profileId: $profileId) {
+      _id
+      name
+      email
+      skills
+    }
+  }
+`;
+
+export const ME = `
+  query me {
+    me {
+      _id
+      name
+      email
+      skills
+    }
+  }
+`;
+
+export const THOUGHTS = `
+  query thoughts {
+    thoughts {
+      _id
+      name
+      thought
+    }
+  }
+`;
+
+export const THOUGHT = `
+  query thought($thoughtId: ID!) {
+    thought(thoughtId: $thoughtId) {
+      _id
+      name
+      thought
+    }
+  }
+`;
