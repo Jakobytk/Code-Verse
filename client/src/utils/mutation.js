1323a2cec795cae8c232e2d5c6696f6d34732869
jakobytk <you@example.com>
2024-03-05 20:02:55 -0600

    apollo client

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,4 +1,6 @@
-export const ADD_PROFILE = `
+import { gql } from  '@apollo/client';
+
+export const ADD_PROFILE = gql`
   mutation addProfile($name: String!, $email: String!, $password: String!) {
     addProfile(name: $name, email: $email, password: $password) {
       token
@@ -12,7 +14,7 @@ export const ADD_PROFILE = `
   }
 `;
 
-export const LOGIN = `
+export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
@@ -26,7 +28,7 @@ export const LOGIN = `
   }
 `;
 
-export const ADD_SKILL = `
+export const ADD_SKILL = gql`
   mutation addSkill($profileId: ID!, $skill: String!) {
     addSkill(profileId: $profileId, skill: $skill) {
       _id
@@ -37,7 +39,7 @@ export const ADD_SKILL = `
   }
 `;
 
-export const REMOVE_PROFILE = `
+export const REMOVE_PROFILE = gql`
   mutation removeProfile {
     removeProfile {
       _id
@@ -45,7 +47,7 @@ export const REMOVE_PROFILE = `
   }
 `;
 
-export const REMOVE_SKILL = `
+export const REMOVE_SKILL = gql`
   mutation removeSkill($skill: String!) {
     removeSkill(skill: $skill) {
       _id
@@ -56,7 +58,7 @@ export const REMOVE_SKILL = `
   }
 `;
 
-export const ADD_THOUGHT = `
+export const ADD_THOUGHT = gql`
   mutation addThought($thoughtId: ID!, $thought: String!) {
     addThought(thoughtId: $thoughtId, thought: $thought) {
       _id
@@ -65,7 +67,7 @@ export const ADD_THOUGHT = `
   }
 `;
 
-export const REMOVE_THOUGHT = `
+export const REMOVE_THOUGHT = gql`
   mutation removeThought($thought: String!) {
     removeThought(thought: $thought) {
       _id
