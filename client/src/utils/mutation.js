e6e5545a196c687add6b10b58e0e6d1e620cf9c0
jakobytk <you@example.com>
2024-03-07 21:32:37 -0600

    working product

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,77 +1,56 @@
-import { gql } from  '@apollo/client';
+import { gql } from '@apollo/client';
 
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
-      token
-      profile {
-        _id
-        name
-        email
-        skills
-      }
-    }
-  }
-`;
-
-export const LOGIN = gql`
+export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      profile {
+      user {
         _id
-        name
-        email
-        skills
+        username
       }
     }
   }
 `;
 
-export const ADD_SKILL = gql`
-  mutation addSkill($profileId: ID!, $skill: String!) {
-    addSkill(profileId: $profileId, skill: $skill) {
-      _id
-      name
-      email
-      skills
-    }
-  }
-`;
-
-export const REMOVE_PROFILE = gql`
-  mutation removeProfile {
-    removeProfile {
-      _id
-    }
-  }
-`;
-
-export const REMOVE_SKILL = gql`
-  mutation removeSkill($skill: String!) {
-    removeSkill(skill: $skill) {
-      _id
-      name
-      email
-      skills
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
 
 export const ADD_THOUGHT = gql`
-  mutation addThought($thoughtId: ID!, $thought: String!) {
-    addThought(thoughtId: $thoughtId, thought: $thought) {
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
       _id
-      name
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
     }
   }
 `;
 
-export const REMOVE_THOUGHT = gql`
-  mutation removeThought($thought: String!) {
-    removeThought(thought: $thought) {
+export const ADD_COMMENT = gql`
+  mutation addComment($thoughtId: ID!, $commentText: String!) {
+    addComment(thoughtId: $thoughtId, commentText: $commentText) {
       _id
-      name
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
     }
   }
 `;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,42 +1,47 @@
 const typeDefs = `
-  type Profile {
-    _id: ID!
-    name: String!
-    email: String!
-    password: String!
-    skills: [String]!
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    thoughts: [Thought]!
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    thoughtAuthor: String
+    createdAt: String
+    comments: [Comment]!
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
   }
 
   type Auth {
     token: ID!
-    profile: Profile
-  }
-
-  type Thought {
-    _id: ID!
-    name: String!
-    thought: String!
+    user: User
   }
 
   type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile!
-    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
-    me: Profile
-    thoughts: [Thought]!
-    thought(thoughtId: ID!): Thought!
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(thoughtId: ID!): Thought
+    me: User
   }
 
   type Mutation {
-    addProfile(name: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile: Profile
-    removeSkill(skill: String!): Profile
-
-    addThought(thoughtId: ID!, thought: String!): Profile
-    removeThought(thought: String!): Thought
+    addThought(thoughtText: String!): Thought
+    addComment(thoughtId: ID!, commentText: String!): Thought
+    removeThought(thoughtId: ID!): Thought
+    removeComment(thoughtId: ID!, commentId: ID!): Thought
   }
 `;
 
